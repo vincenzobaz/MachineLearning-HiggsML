a7a2d45168ffee54cd56b231f939f5ed765ad707
amaumaury <amaurycombes@hotmail.fr>
2017-10-18 08:43:08 +0200

    Add version 2 of cross validation

--- implementations.py
+++ implementations.py
@@ -1,4 +1,5 @@
 import numpy as np
+import proj1_helpers as helper
 
 
 def mse(y, tx, w):
@@ -130,6 +131,7 @@ def cross_validation(y, x, k_fold, regression_f, degree, seed=1, compute_loss=rm
     loss_tr = 0
     loss_te = 0
     weigths = np.zeros((degree + 1) * x.shape[1])  # if quadratic, three parameters....
+    print(weigths.shape)
 
     for i in range(k_fold):
         tmp_loss_tr, tmp_loss_te, w = cross_validation_step(i)
@@ -138,3 +140,55 @@ def cross_validation(y, x, k_fold, regression_f, degree, seed=1, compute_loss=rm
         weigths += w
 
     return loss_tr / k_fold, loss_te / k_fold, weigths / k_fold
+
+def cross_validation_v2(y, x, k_fold, regression_f, degree, seed=1, compute_loss=rmse):
+    """
+    Computes weights, training and testing error
+
+    regression_f is a regressiong function only accepting y and the tx matrix.
+    In case of ridge regression (or any other function needing more paremeters),
+    the additional ones can be curried.
+    e.g. f = lambda y, tx: ridge_regression(y, tx, lambda_, compute_loss=...)
+    """
+
+    def build_k_indices():
+        """build k indices for k-fold."""
+        num_row = y.shape[0]
+        interval = int(num_row / k_fold)
+        np.random.seed(seed)
+        indices = np.random.permutation(num_row)
+        k_indices = [indices[k * interval: (k + 1) * interval]
+                     for k in range(k_fold)]
+        return np.array(k_indices)
+
+    k_indices = build_k_indices()
+
+    def cross_validation_step(k):
+        """Computes one iteration of k-fold cross validation"""
+        test_x, test_y = x[k_indices[k]], y[k_indices[k]]
+        train_indices = k_indices[[i for i in range(len(k_indices)) if i != k]]
+        train_indices = np.ravel(train_indices)
+        tx = polynomial_enhancement(x[train_indices], degree)
+        w, loss_tr = regression_f(y[train_indices], tx)
+
+        empirical_y_test = helper.predict_labels(w, polynomial_enhancement(test_x, degree))
+        sum_vector = (empirical_y_test + test_y)
+        accuracy = sum_vector[np.where(sum_vector != 0)].size / test_y.size
+
+        loss_te = compute_loss(
+            test_y, polynomial_enhancement(test_x, degree), w)
+        return loss_tr, loss_te, w, accuracy
+
+    loss_tr = []
+    loss_te = []
+    weigths = []  # if quadratic, three parameters....
+    accuracy = []
+
+    for i in range(k_fold):
+        tmp_loss_tr, tmp_loss_te, w, tmp_accuracy = cross_validation_step(i)
+        loss_tr.append(tmp_loss_tr)
+        loss_te.append(tmp_loss_te)
+        accuracy.append(tmp_accuracy)
+        weigths.append(w)
+
+    return accuracy, loss_tr, loss_te, weigths
