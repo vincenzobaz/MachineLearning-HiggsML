9833ff9737f6cd598d3a5b57918f053187746b2c
vincenzobaz <bazzucchi.vincenzo@gmail.com>
2017-10-26 13:05:05 +0200

    Refactor

--- least_squares.py
+++ least_squares.py
@@ -1,9 +1,10 @@
 import numpy as np
 import implementations as imp
+import minimizers
+from Preprocessor import EmptyPreprocessor()
 
 class LeastSquares:
-
-    def __init__(self, preprocess, solver='pseudo', **kwargs):
+    def __init__(self, preprocessor=EmptyPreprocessor(), solver='pseudo', **kwargs):
         """Class constructor
 
         Keyword arguments:
@@ -20,12 +21,12 @@ class LeastSquares:
         losses = ls.losses # only if used iterative solver (gradient, stochastic)
         predictions = ls.predict(x_test)
         """
-        self.preprocess_f = preprocess
+        self.preprocessor = preprocessor
         self.solver = solver
         self.solver_args = kwargs
 
     def train(self, y, x):
-        processed = self.preprocess_f(x)
+        processed = self.preprocessor.preprocess_train(x)
 
         # Switch statement à-la python
         chooser = {
@@ -34,7 +35,11 @@ class LeastSquares:
             'gradient': self._gradient_s,
             'stochastic._gradient': self._sgd_s
         }
-        chooser[self.solver](y, x)
+        chooser[self.solver](y, processed)
+
+    def predict(self, x_test):
+        ready = self.preprocessor.preprocess_test(x_test)
+        return x_test @ self.model
 
     def _direct_s(self, y, tx):
         self.model = np.linalg.inv(tx.T @ tx) @ tx.T @ y
@@ -45,29 +50,29 @@ class LeastSquares:
         self.model = V.T @ np.diag(1 / S) @ U.T @ y
         self.loss = self.solver_args.get('compute_loss', imp.rmse)(y, tx, self.model)
 
-    def _compute_gradient(self, y, tx, w):
+    @staticmethod
+    def compute_gradient(y, tx, w):
+        y = np.reshape(y, (len(y), 1))
         e = y - (tx @ w)
         return -1 / len(y) * (tx.T @ e)
 
     def _gradient_s(self, y, tx):
         niter = 0
         w = self.solver_args.get('w0', np.zeros((tx.shape[1], 1)))
-        prev_loss = 0
-        next_loss = np.inf
         max_iter = self.solver_args.get('max_iters', 100)
         threshold = self.solver_args.get('threshold', 10**(-3))
         self.losses = []
+        gamma = self.solver_args['gamma']
+        lambda_ = self.solver_args.get('lambda_')
 
-        while niter < max_iter and np.abs(next_loss - prev_loss) > threshold:
-            prev_loss = next_loss
-            grad = self._compute_gradient(y, tx, w)
-            w -= self.solver_args['gamma'] * grad
-            next_loss = self.solver_args.get('compute_loss', imp.rmse)(y, tx, w)
-            self.losses.append(next_loss)
-            niter += 1
+        niter, losses, w = minimizers.gradient_descent(y, tx, w, max_iter, threshold,
+                                                       LeastSquares.compute_gradient,
+                                                       self.solver_args.get('compute_loss', imp.rmse),
+                                                       gamma, lambda_)
 
+        self.niter = niter
         self.model = w
-        self.loss = next_loss
+        self.losses = losses
 
     def _sgd_s(self, y, tx):
         batch_size = self.solver_args['batch_size']
@@ -78,15 +83,11 @@ class LeastSquares:
         self.losses = []
 
         for batch_y, batch_tx in imp.batch_iter(y, tx, num_batches, shuffle):
-            grad = self._compute_gradient(y, tx, w)
+            grad = LeastSquares.compute_gradient(y, tx, w)
             w -= self.solver_args['gamma'] * grad
             loss = self.solver_args.get('compute_loss', imp.rmse)(y, tx, w)
             self.losses.append(loss)
 
         self.model = w
         self.loss = loss
-
-    def predict(self, x_test):
-        ready = self.preprocess_f(x_test)
-        return x_test @ self.model
 
