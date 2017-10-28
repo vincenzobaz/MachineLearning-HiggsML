6ab11763526c032e383a2b316fb10b0f90c415b3
vincenzobaz <bazzucchi.vincenzo@gmail.com>
2017-10-28 10:33:39 +0200

    Make test data optional

--- preprocessing.py
+++ preprocessing.py
@@ -1,27 +1,27 @@
 import numpy as np
 import run
 
-def preprocess(x_tr, x_te):
+def preprocess(x_tr, x_te=None):
     x_train = x_tr.copy()
 
     stds = np.std(x_train, axis=0)
-    #useless = np.where(stds < 0.7)[0]
-    zero = np.where(stds == 0)[0]
 
-    #deleted_cols_ids = np.union1d(zero, useless)
-    deleted_cols_ids = zero
+    deleted_cols_ids = np.where(stds < 0.7)
 
     x_train = np.delete(x_train, deleted_cols_ids, axis=1)
     run.mean_spec(x_train)
     x_train = run.standardize(x_train)
 
 
-    x_test = x_te.copy()
+    if x_te is not None:
+        x_test = x_te.copy()
 
-    stds = np.std(x_test, axis=0)
-    x_test = np.delete(x_test, deleted_cols_ids, axis=1)
-    run.mean_spec(x_test)
-    x_test = run.standardize(x_test)
+        stds = np.std(x_test, axis=0)
+        x_test = np.delete(x_test, deleted_cols_ids, axis=1)
+        run.mean_spec(x_test)
+        x_test = run.standardize(x_test)
 
-    return x_train, x_test
+        return x_train, x_test
+    else:
+        return x_train
 
